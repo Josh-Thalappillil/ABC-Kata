a7b96d69bcd02355954b7800c2b1f675d5325891
Josh <jthalappillil7@gmail.com>
2023-04-08 16:06:45 +1000

    Created Block and Game Engine Class

---
+++ Blocks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Kata
+{
+
+    public class Blocks
+    {
+        private List <string> _blocks = new List <string> ()
+        {
+            "B O", "X K", "D Q", "C P",
+            "N A", "G T", "R E", "T G",
+            "Q D", "F S", "J W", "H U", 
+            "V I", "A N", "O B", "E R", 
+            "F S", "L Y", "P C", "Z M"
+        }
+    }
+}
